9a80b3d3965d658e648883a09a376b4fa1aefa9d
C107165108 <C107165108@nkust.edu.tw>
2024-09-26 15:40:07 +0800

    Locofy import

---
+++ Wrapper.ts
@@ -0,0 +1,9 @@
+// src/app/wrapper.component.ts
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wrapper',
+  template: `<ng-content></ng-content>`,
+  styles: []
+})
+export class WrapperComponent {}
